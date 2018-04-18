5547b2f4cd9809570e4305344e27b3885923a21e
kiwikierab <kierangio123@gmail.com>
2018-04-19 07:47:36 +1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -224,8 +224,9 @@ EOF
 while true
   do
     sleep 30
-    #/bin/nc 127.0.0.1 30002 | /bin/nc radar1.ddns.net $RECEIVERPORT
-    /usr/bin/socat -u TCP:localhost:30002 TCP:radar1.ddns.net:$RECEIVERPORT
+    #/bin/nc 127.0.0.1 30005 | /bin/nc radar1.ddns.net $RECEIVERPORT
+    /usr/bin/socat -u TCP:localhost:30005 TCP:radar1.ddns.net:$RECEIVERPORT
+    /usr/bin/socat -u TCP:localhost:30104 TCP:radar1.ddns.net:30104
   done
 EOF
 
