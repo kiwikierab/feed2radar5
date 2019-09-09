671dcbb5954014d802251a75d649886d30ce465c
ADSBexchange.com <34561158+adsbxchange@users.noreply.github.com>
2019-09-08 19:08:18 -0700

    MLAT only
    mlat forward only

--- setup_mlat_forward.sh
+++ setup_mlat_forward.sh
@@ -36,18 +36,14 @@ MLATCLIENTTAG="v0.2.6"
 
 ## WHIPTAIL DIALOGS
 
-BACKTITLETEXT="Radar NZ Setup Script w/ ADSBX installer"
+BACKTITLETEXT="Radar NZ MLAT Forward"
 
-whiptail --backtitle "$BACKTITLETEXT" --title "$BACKTITLETEXT" --yesno "Thanks for choosing to share your data with Radar5!\n\nradar5.nz is a co-op of ADS-B/Mode S/MLAT feeders from around the New Zealand. This script will configure your current your ADS-B receiver to share your feeders data with Radar5 and ADS-B Exchange.\n\nWould you like to continue setup?" 13 78
+whiptail --backtitle "$BACKTITLETEXT" --title "$BACKTITLETEXT" --yesno "This script should be ran AFTER setup.sh. It will setup MLAT forwarding from ADSBx mlat-client.\n\nWould you like to continue setup?" 13 78
 CONTINUESETUP=$?
 if [ $CONTINUESETUP = 1 ]; then
     exit 0
 fi
 
-ADSBEXCHANGEUSERNAME=$(whiptail --backtitle "$BACKTITLETEXT" --title "Radar5 Feeder Name" --nocancel --inputbox "\nPlease enter your Radar5 feeder name.\n\nIf you have more than one receiver, this username should be unique.\nExample: \"username-01\", \"username-02\", etc." 12 78 3>&1 1>&2 2>&3)
-RECEIVERLATITUDE=$(whiptail --backtitle "$BACKTITLETEXT" --title "Receiver Latitude" --nocancel --inputbox "\nEnter your receivers latitude." 9 78 3>&1 1>&2 2>&3)
-RECEIVERLONGITUDE=$(whiptail --backtitle "$BACKTITLETEXT" --title "Receiver Longitude" --nocancel --inputbox "\nEnter your recivers longitude." 9 78 3>&1 1>&2 2>&3)
-RECEIVERALTITUDE=$(whiptail --backtitle "$BACKTITLETEXT" --title "Receiver Longitude" --nocancel --inputbox "\nEnter your recivers altitude." 9 78 3>&1 1>&2 2>&3)
 RECEIVERPORT=$(whiptail --backtitle "$BACKTITLETEXT" --title "RadarNZ Feed Port" --nocancel --inputbox "\nChange only if you were assigned a custom feed port.\nFor most all users it is required this port remain set to port 4995." 10 78 "4995" 3>&1 1>&2 2>&3)
 
 
@@ -73,36 +69,9 @@ fi
 
     # BUILD AND CONFIGURE THE MLAT-CLIENT PACKAGE
 
-    echo "INSTALLING PREREQUISITE PACKAGES" >> $LOGFILE
+    echo "INSTALLING SOCAT IF NEEDED" >> $LOGFILE
     echo "--------------------------------------" >> $LOGFILE
     echo "" >> $LOGFILE
-
-
-    # Check that the prerequisite packages needed to build and install mlat-client are installed.
-    if [ $(dpkg-query -W -f='${STATUS}' build-essential 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-        sudo apt-get install -y build-essential >> $LOGFILE  2>&1
-    fi
-
-    echo 10
-    sleep 0.25
-
-    if [ $(dpkg-query -W -f='${STATUS}' debhelper 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-        sudo apt-get install -y debhelper >> $LOGFILE  2>&1
-    fi
-
-    echo 16
-    sleep 0.25
-
-    if [ $(dpkg-query -W -f='${STATUS}' python 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-        sudo apt-get install -y python >> $LOGFILE  2>&1
-    fi
-    
-    if [ $(dpkg-query -W -f='${STATUS}' python3-dev 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-        sudo apt-get install -y python3-dev >> $LOGFILE  2>&1
-    fi
-
-    echo 22
-    sleep 0.25
 
     if [ $(dpkg-query -W -f='${STATUS}' socat 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
         sudo apt-get install -y socat >> $LOGFILE  2>&1
@@ -111,178 +80,11 @@ fi
     echo 28
     sleep 0.25
 
-    echo "" >> $LOGFILE
-    echo " BUILD AND INSTALL MLAT-CLIENT" >> $LOGFILE
-    echo "-----------------------------------" >> $LOGFILE
-    echo "" >> $LOGFILE
-
-    # Check if the mlat-client git repository already exists.
-    if [ -d mlat-client ] && [ -d mlat-client/.git ]; then
-        # If the mlat-client repository exists update the source code contained within it.
-        cd mlat-client >> $LOGFILE
-        git pull >> $LOGFILE 2>&1
-        git checkout tags/$MLATCLIENTTAG >> $LOGFILE 2>&1
-    else
-        # Download a copy of the mlat-client repository since the repository does not exist locally.
-        git clone https://github.com/mutability/mlat-client.git >> $LOGFILE 2>&1
-        cd mlat-client >> $LOGFILE 2>&1
-        git checkout tags/$MLATCLIENTTAG >> $LOGFILE 2>&1
-    fi
-
-    echo 34
-    sleep 0.25
-
-    # Build and install the mlat-client package.
-    dpkg-buildpackage -b -uc >> $LOGFILE 2>&1
-    cd .. >> $LOGFILE
-    sudo dpkg -i mlat-client_${MLATCLIENTVERSION}*.deb >> $LOGFILE
-
-    echo 40
-    sleep 0.25
-
-    echo "" >> $LOGFILE
-    echo " CREATE AND CONFIGURE MLAT-CLIENT STARTUP SCRIPTS" >> $LOGFILE
-    echo "------------------------------------------------------" >> $LOGFILE
-    echo "" >> $LOGFILE
-
-    # Create the mlat-client maintenance script.
-    tee adsbexchange-mlat_maint.sh > /dev/null <<EOF
-#!/bin/sh
-while true
-  do
-    sleep 30
-    /usr/bin/mlat-client --input-type dump1090 --input-connect localhost:30005 --lat $RECEIVERLATITUDE --lon $RECEIVERLONGITUDE --alt $RECEIVERALTITUDE --user $ADSBEXCHANGEUSERNAME --server feed.adsbexchange.com:31090 --no-udp --results beast,connect,localhost:30104 --results beast,listen,30105
-  done
-EOF
-
-    echo 46
-    sleep 0.25
-
-    # Set execute permissions on the mlat-client maintenance script.
-    chmod +x adsbexchange-mlat_maint.sh >> $LOGFILE
-
-    echo 52
-    sleep 0.25
-
-    # Add a line to execute the mlat-client maintenance script to /etc/rc.local so it is started after each reboot if one does not already exist.
-    if ! grep -Fxq "$PWD/adsbexchange-mlat_maint.sh &" /etc/rc.local; then
-        LINENUMBER=($(sed -n '/exit 0/=' /etc/rc.local))
-        ((LINENUMBER>0)) && sudo sed -i "${LINENUMBER[$((${#LINENUMBER[@]}-1))]}i $PWD/adsbexchange-mlat_maint.sh &\n" /etc/rc.local >> $LOGFILE
-    fi
-
-    echo 58
-    sleep 0.25
-
-    echo "" >> $LOGFILE
-    echo " CREATE AND CONFIGURE NETCAT STARTUP SCRIPTS" >> $LOGFILE
+        echo "" >> $LOGFILE
+    echo " CREATE AND CONFIGURE MLAT FORWARD SCRIPTS" >> $LOGFILE
     echo "-------------------------------------------------" >> $LOGFILE
     echo "" >> $LOGFILE
 
-    # Kill any currently running instances of the adsbexchange-mlat_maint.sh script.
-    PIDS=`ps -efww | grep -w "adsbexchange-mlat_maint.sh" | awk -vpid=$$ '$2 != pid { print $2 }'`
-    if [ ! -z "$PIDS" ]; then
-        sudo kill $PIDS >> $LOGFILE
-        sudo kill -9 $PIDS >> $LOGFILE
-    fi
-
-    echo 64
-    sleep 0.25
-
-    # Execute the mlat-client maintenance script.
-    sudo nohup $PWD/adsbexchange-mlat_maint.sh > /dev/null 2>&1 & >> $LOGFILE
-
-    echo 70
-    sleep 0.25
-
-    # SETUP NETCAT TO SEND DUMP1090 DATA TO ADS-B EXCHANGE
-
-    # Create the netcat maintenance script.
-    tee adsbexchange-netcat_maint.sh > /dev/null <<EOF
-#!/bin/sh
-while true
-  do
-    sleep 30
-    /usr/bin/socat -u TCP:localhost:30005 TCP:feed.adsbexchange.com:30005
-  done
-EOF
-
-    echo 76
-    sleep 0.25
-
-    # Set permissions on the file adsbexchange-netcat_maint.sh.
-    chmod +x adsbexchange-netcat_maint.sh >> $LOGFILE
-
-    echo 82
-    sleep 0.25
-
-    # Add a line to execute the netcat maintenance script to /etc/rc.local so it is started after each reboot if one does not already exist.
-    if ! grep -Fxq "$PWD/adsbexchange-netcat_maint.sh &" /etc/rc.local; then
-        lnum=($(sed -n '/exit 0/=' /etc/rc.local))
-        ((lnum>0)) && sudo sed -i "${lnum[$((${#lnum[@]}-1))]}i $PWD/adsbexchange-netcat_maint.sh &\n" /etc/rc.local >> $LOGFILE
-    fi
-
-    echo 88
-    sleep 0.25
-
-    # Kill any currently running instances of the adsbexchange-netcat_maint.sh script.
-    PIDS=`ps -efww | grep -w "adsbexchange-netcat_maint.sh" | awk -vpid=$$ '$2 != pid { print $2 }'`
-    if [ ! -z "$PIDS" ]; then
-        sudo kill $PIDS >> $LOGFILE
-        sudo kill -9 $PIDS >> $LOGFILE
-    fi
-
-    echo 94
-    sleep 0.25
-
-    # Execute the netcat maintenance script.
-    sudo nohup $PWD/adsbexchange-netcat_maint.sh > /dev/null 2>&1 & >> $LOGFILE
-    echo 100
-    sleep 0.25
-    
-    #
-    # Create the radar.nz dump1090 script.
-    #
-    tee radarnz-netcat_maint.sh > /dev/null <<EOF
-#!/bin/sh
-while true
-  do
-    sleep 30
-    /usr/bin/socat -u TCP:localhost:30005 TCP:feed.radar5.nz:$RECEIVERPORT
-   done
-EOF
-
-    echo 76
-    sleep 0.25
-
-    # Set permissions on the file radarnz-netcat_maint.sh.
-    chmod +x radarnz-netcat_maint.sh >> $LOGFILE
-
-    echo 82
-    sleep 0.25
-
-    # Add a line to execute the netcat maintenance script to /etc/rc.local so it is started after each reboot if one does not already exist.
-    if ! grep -Fxq "$PWD/radarnz-netcat_maint.sh &" /etc/rc.local; then
-        lnum=($(sed -n '/exit 0/=' /etc/rc.local))
-        ((lnum>0)) && sudo sed -i "${lnum[$((${#lnum[@]}-1))]}i $PWD/radarnz-netcat_maint.sh &\n" /etc/rc.local >> $LOGFILE
-    fi
-
-    echo 88
-    sleep 0.25
-
-    # Kill any currently running instances of the rasarnx_ netcat_maint.sh script.
-    PIDS=`ps -efww | grep -w "radarnz-netcat_maint.sh" | awk -vpid=$$ '$2 != pid { print $2 }'`
-    if [ ! -z "$PIDS" ]; then
-        sudo kill $PIDS >> $LOGFILE
-        sudo kill -9 $PIDS >> $LOGFILE
-    fi
-
-    echo 94
-    sleep 0.25
-
-    # Execute the netcat maintenance script.
-    sudo nohup $PWD/radarnz-netcat_maint.sh > /dev/null 2>&1 & >> $LOGFILE
-    echo 100
-    sleep 0.25
 #
 #  Forward MLAT to radarnz script from MLAT listener
 # 
@@ -328,11 +130,11 @@ EOF
     echo 100
     sleep 0.25
 
-} | whiptail --backtitle "$BACKTITLETEXT" --title "Setting Up ADS-B Exchange Feed & RadarNZ"  --gauge "\nSetting up your receiver to feed Radar5.\nThe setup process may take awhile to complete..." 8 60 0
+} | whiptail --backtitle "$BACKTITLETEXT" --title "Setting MLAT forwarding for RadarNZ"  --gauge "\nSetting up your receiver to feed Radar5.\nThe setup process may take awhile to complete..." 8 60 0
 
 ## SETUP COMPLETE
 
 # Display the thank you message box.
-whiptail --title "Radar5 Setup Script" --msgbox "\nSetup is now complete.\n\nYour feeder should now be feeding data to Radar5.\nThanks again for choosing to share your data with Radar5!\n\nIf you have questions \n\nhttps://radar5.nz" 17 73
+whiptail --title "Radar5 Setup Script" --msgbox "\nSetup is now complete.\n\nYour feeder is now sending MLAT from ADSBx to Radar5.nz.\nThanks again for choosing to share your data with Radar5!\n\nIf you have questions \n\nhttps://radar5.nz" 17 73
 
 exit 0
